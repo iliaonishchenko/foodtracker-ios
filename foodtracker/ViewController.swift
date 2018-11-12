5c388f276ea0fd03e33bbe622e0d9afc5ce5288e
Ilia Onishchenko <wopqw@yandex.ru>
2018-11-13 02:08:03 +0300

    Initial commit

---
+++ foodtracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  foodtracker
+//
+//  Created by Ilya Onishenko on 13/11/2018.
+//  Copyright © 2018 Ilya Onishenko. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}

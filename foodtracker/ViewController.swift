c2e90afcc944797895df66f92d204bb03bb0b5b3
Ilia Onishchenko <wopqw@yandex.ru>
2018-12-07 01:30:18 +0300

    [WIP] Add ImageView with taps[2]

--- foodtracker/ViewController.swift
+++ foodtracker/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var mealLabel: UILabel!
@@ -37,11 +37,31 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         mealLabel.text = textField.text
     }
-
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        photoImageVIew.image = selectedImage
+        dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
     //MARK: Actions
     @IBAction func setDeaultMealText(_ sender: UIButton) {
         mealLabel.text = "Default Text"
     }
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField1.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
 }
